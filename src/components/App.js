2013cfd967df1de0baea1331abcb4b2050f342be
Noah Cowan <nsncowan@gmail.com>
2023-11-06 15:18:57 -0800

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Header } from "./layout/Header";
+import { Content } from "./layout/Content";
+
+export const App = () => (
+    <div>
+      <Header />
+      <Content />
+    </div>
+  );
+
+
+export default App;
