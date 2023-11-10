78063ded3314a50e10417d99c7ff27bca6c679d9
Noah Cowan <nsncowan@gmail.com>
2023-11-10 15:20:42 -0800

    add to custom hooks

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@
 Note that Firebase is not prescriptive about what we name our configuration file or where we located, 
 so it's up to us to pick a name and location that is sensible and intuitive. */
 
+/* 
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from "firebase/auth";
@@ -19,4 +20,23 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-export { db, auth };
+export { db, auth }; 
+*/
+
+
+// ALT VERSION
+
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = firebase.initializeApp({
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+});
+
+export { firebaseConfig as firebase };
